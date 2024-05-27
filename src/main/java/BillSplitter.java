3a8073df741b55c4a0547e1e5cc370252624338f
Grigory Ulanov <gr.ulanov@gmail.com>
2024-05-27 19:51:15 +0300

    Fix PR comments

---
+++ src/main/java/BillSplitter.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+import exeptions.IncorrectGuestsNumber;
+import models.Bill;
+
+public final class BillSplitter {
+
+    void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        BillCalculator calculator = null;
+        BillReader billReader = new BillReader();
+        BillPrinter billPrinter = new BillPrinter();
+
+        System.out.println("Вас приветствует программа для разделения счета!");
+        System.out.println("================================================");
+
+        while (calculator == null) {
+            System.out.println("\nУкажите количество гостей, между которыми нужно разделить счет:");
+            try {
+                String input = scanner.next().trim();
+                int guestsNumber = Integer.parseInt(input);
+                calculator = new BillCalculator(guestsNumber);
+            } catch (IncorrectGuestsNumber e) {
+                System.out.println(String.format("Некорректный ввод! %s.", e.getMessage()));
+            } catch (Exception e) {
+                System.out.println("Некорректный ввод! Допустимо вводить только числа.");
+            }
+        }
+
+        Bill bill = billReader.read();
+        billPrinter.print(bill);
+        calculator.calculate(bill);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,6 @@
-import java.util.Scanner;
-
-import exeptions.IncorrectGuestsNumber;
-import models.Bill;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        BillCalculator calculator = null;
-        BillReader billReader = new BillReader();
-        BillPrinter billPrinter = new BillPrinter();
-
-        System.out.println("Вас приветствует программа для разделения счета!");
-        System.out.println("================================================");
-
-        while (calculator == null) {
-            System.out.println("\nУкажите количество гостей, между которыми нужно разделить счет:");
-            try {
-                String input = scanner.next().trim();
-                int guestsNumber = Integer.parseInt(input);
-                calculator = new BillCalculator(guestsNumber);
-            } catch (IncorrectGuestsNumber e) {
-                System.out.println(String.format("Некорректный ввод! %s.", e.getMessage()));
-            } catch (Exception e) {
-                System.out.println("Некорректный ввод! Допустимо вводить только числа.");
-            }
-        }
-
-        Bill bill = billReader.read();
-        billPrinter.print(bill);
-        calculator.calculate(bill);
+        BillSplitter billSplitter = new BillSplitter();
+        billSplitter.start();
     }
 }
